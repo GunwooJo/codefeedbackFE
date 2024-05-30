5094fd9b8b75aec43b60e58389b9f1ef0eea3b57
Ourumo <ss8638@naver.com>
2024-05-30 13:29:09 +0900

    feat: 유저 로그인 페이지 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import UserJoin from "./pages/UserJoin";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import UserLogin from "./pages/UserLogin";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route path="/user/join" element={<UserJoin />} />
+                <Route path="/user/login" element={<UserLogin />} />
             </Routes>
         </BrowserRouter>
     </div>
