39aa807895c2f3e740244d00baff27b29889d06f
GunwooJo <kanggi1997@naver.com>
2024-06-08 17:06:30 +0900

    fix: module.css 적용

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,54 +1,36 @@
 import * as React from 'react';
-import '../styles/Home.css'
-//import axios from 'axios';
-//import {useNavigate} from "react-router-dom";
-import { Decrypt } from '../components/HashString';
-import {useLocation, useParams} from 'react-router-dom';
+import styles from '../styles/Home.module.css'
+import {useLocation} from 'react-router-dom';
 
 export default function Home() {
-    // const { user } = useParams();
     const location = useLocation();
     const user = location.state.user;
-    const [decryptedUser, setDecryptedUser] = React.useState([])
 
     React.useEffect(() => {
         alert(`${user.nickname}님 환영합니다.`);  //테스트용 코드. 해당 user.nickname, user.email로 유저 정보 조회 가능합니다.
 
     },[])
-    // React.useEffect(() => {
-    //     try {
-    //         setDecryptedUser(Decrypt(user).split('&'));
-    //         console.log(decryptedUser);
-    //     } catch(error) {
-    //         console.log(error);
-    //     }
-    // }, []);
-
-    // React.useEffect(() => {
-    //     if(decryptedUser[1] !== undefined)
-    //         alert("Hello!\nuser: " + decryptedUser[1]);
-    // }, [decryptedUser]);
 
     return (
-        <div class="main-container">
-            <div class="navigation-bar">
-                <button class="submit-button">submit</button>
-                <button class="navigation-button">my profile</button>
-                <button class="navigation-button">board list</button>
+        <div>
+            <div>
+                <button class={styles.submitButton}>submit</button>
+                <button class={styles.navigationButton}>my profile</button>
+                <button class={styles.navigationButton}>board list</button>
             </div>
-            <div class="chat-container">
-                <div class="message received">
-                    <div class="message-content">
+            <div class={styles.chatContainer}>
+                <div class={styles.messageReceived}>
+                    <div class={styles.messageContent}>
                         <p>Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!</p>
                     </div>
                 </div>
-                <div class="message sent">
-                    <div class="message-content">
+                <div class={styles.messageSent}>
+                    <div class={styles.messageContent}>
                         <p>Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!Test!</p>
                     </div>
                 </div>
             </div>
-                <div class="input-container">
+                <div class={styles.inputContainer}>
                     <form>
                         <textarea rows="8"/>
                         <input type='submit'/>
