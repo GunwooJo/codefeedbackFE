3cf23de42a0c077a0c203bae11b28c50ebbca446
GunwooJo <kanggi1997@naver.com>
2024-06-08 17:52:29 +0900

    feat: 로그인한 유저 정보 context api로 관리

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,23 @@ import Home from "./pages/Home";
 import UserEdit from "./pages/UserEdit";
 import UserInfo from "./pages/UserInfo";
 import PostDetail from "./pages/PostDetail";
+import {UserProvider} from "./context/UserContext";
 
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/user/join" element={<UserJoin />} />
-              <Route path="/user/login" element={<UserLogin />} />
-              <Route path="/user/edit" element={<UserEdit/>}/>
-              <Route path="/user/info" element={<UserInfo/>}/>
-              <Route path="/post/:postId" element={<PostDetail/>}/>
-            </Routes>
-        </BrowserRouter>
+        <UserProvider>
+            <BrowserRouter>
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/user/join" element={<UserJoin />} />
+                  <Route path="/user/login" element={<UserLogin />} />
+                  <Route path="/user/edit" element={<UserEdit/>}/>
+                  <Route path="/user/info" element={<UserInfo/>}/>
+                  <Route path="/post/:postId" element={<PostDetail/>}/>
+                </Routes>
+            </BrowserRouter>
+        </UserProvider>
     </div>
   );
 }
