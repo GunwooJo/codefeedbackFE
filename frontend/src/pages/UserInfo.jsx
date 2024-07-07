8f37f5483a38cd384421fda585b93d09adbd0f4b
GunwooJo <kanggi1997@naver.com>
2024-07-08 00:35:35 +0900

    feat: 회원정보 띄울 때 커스텀 훅 사용

--- frontend/src/pages/UserInfo.jsx
+++ frontend/src/pages/UserInfo.jsx
@@ -1,40 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import UserWithdrawal from "../components/UserWithdrawal";
 import styles from '../styles/UserInfo.module.css'
 import styles2 from "../styles/PostDetail.module.css";
+import useSessionCheck from "../hooks/useSessionCheck";
 
 function UserInfo() {
-    const [userInfo, setUserInfo] = useState({
-        email: '',
-        nickname: ''
-    });
+    const {loggedInUser} = useSessionCheck();
     const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
-
-    useEffect(() => {
-
-        async function checkSession() {
-            try {
-                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/session/check`,{withCredentials: true});
-                setUserInfo(response.data.data);
-
-            } catch (error) {
-
-                if (error.response && error.response.status === 401) {
-                    alert("로그인이 필요합니다.");
-                    navigate("/user/login");
-                } else {
-                    console.error(error);
-                    alert("오류가 발생했습니다.");
-                }
-            }
-
-        }
-
-        checkSession();
-    }, [navigate]);
 
     const handleShow = () => setShowModal(true);
     const handleEdit = () => navigate('/user/edit');
@@ -46,11 +20,11 @@ function UserInfo() {
 
             <div>
                 <label>이메일: </label>
-                <span>{userInfo.email}</span>
+                <span>{loggedInUser.email}</span>
             </div>
             <div>
                 <label>닉네임: </label>
-                <span>{userInfo.nickname}</span>
+                <span>{loggedInUser.nickname}</span>
             </div>
             <button onClick={handleEdit} className={styles.navigationButton}>수정</button>
             {/*<button onClick={handleShow} className={styles.navigationButton}>탈퇴</button>*/}
